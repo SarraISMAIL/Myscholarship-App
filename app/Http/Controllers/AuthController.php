c4bdfba4cbfe255e3241c6257d929b56d5ee7b68
Sarra <sarra.ismail@eniso.u-sousse.tn>
2021-09-01 14:40:51 +0100

    Authentication Routes and registration method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -27,14 +28,26 @@ class AuthController extends Controller
         if( $validator->fails()){
             return response(['errors'=> $validator->errors()],422);
         }
+            $user = new User();
+            $user ->first_name = $request->firstName;
+            $user ->last_name = $request->lastName;
+            $user ->email = $request->email;
+            $user ->password = bcrypt($request->password);
+            $user ->save();
 
-        $user = new User();
-        $user ->first_name = $request->firstName;
-       $user ->last_name = $request->lastName;
-       $user ->email = $request->email;
-       $user ->password = bcrypt($request->password);
-       $user ->save();
+            //Token
 
+           $tokenResult= $user->createToken("Personal access Token");
+               $token = $tokenResult->token;
+               $token ->expires_at = carbon::now()->addweeks(1);
+               $token ->save();
+               return response([
+                   'accessToken' => $tokenResult->token,
+                   'tokenType'=> "Bearer",
+                   'expiresAt' =>Carbon::parse($token->expires_at)->todatetimeString();
+
+
+               ],200);
 
 
    }
