06edfdae066d8e2a089b6a9037059136e5548982
Sarra <sarra.ismail@eniso.u-sousse.tn>
2021-09-01 15:48:15 +0100

    register method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,45 +10,53 @@ use Illuminate\Support\Facades\Validator;
 class AuthController extends Controller
 {
 
-   public function register(request $request){
-/*
- *    $table->string('first_name');
-            $table->string('last_name');
-            $table->string('bio')->nullable();
-            $table->string('email');
- * */
+    public function authFailed(){
+        return response('unauthenticated', 401);
+    }
 
-       $validator = Validator::make($request->all(),[
-           'firstName'=> 'required|string|max:255',
-           'lastName'=> 'required|string|max:255',
-           'email'=> 'required|string|email|unique:users|max:255',
-           'password'=>'required|string|min:6|confirmed',
-      ]);
+    public function register(Request $request){
 
-        if( $validator->fails()){
-            return response(['errors'=> $validator->errors()],422);
+        $validator = Validator::make($request->all(),[
+            'firstName' => 'required|string|max:255',
+            'lastName' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users|max:255',
+            'password' => 'required|string|max:255|min:6|confirmed'
+        ]);
+
+        if($validator->fails()){
+            return response(['errors' => $validator->errors()], 422);
         }
-            $user = new User();
-            $user ->first_name = $request->firstName;
-            $user ->last_name = $request->lastName;
-            $user ->email = $request->email;
-            $user ->password = bcrypt($request->password);
-            $user ->save();
 
-            //Token
+        $user = new User();
+        $user->first_name = $request->firstName;
+        $user->last_name = $request->lastName;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
 
-           $tokenResult= $user->createToken("Personal access Token");
-               $token = $tokenResult->token;
-               $token ->expires_at = carbon::now()->addweeks(1);
-               $token ->save();
-               return response([
-                   'accessToken' => $tokenResult->token,
-                   'tokenType'=> "Bearer",
-                   'expiresAt' =>Carbon::parse($token->expires_at)->todatetimeString();
+        return $this->getResponse($user);
+    }
+
+///login logout
+///
+///
+    public function user(Request $request){
+        return $request->user();
+    }
+
+    private function getResponse(User $user){
+
+        $tokenResult =   $user->createToken("Personal Access Token");
+        $token = $tokenResult->token;
+        $token->expires_at = Carbon::now()->addWeeks(1);
+        $token->save();
 
 
-               ],200);
+        return  response([
+            'accessToken' => $tokenResult->accessToken,
+            'tokenType' => "Bearer",
+            'expiresAt' => Carbon::parse($token->expires_at)->toDateTimeString()
+        ],200);
+    }
 
-
-   }
 }
